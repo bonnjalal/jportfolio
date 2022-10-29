6aee7112572616c27d3d206a35474259d6479bd4
Abdelilah Jalal <bonnjalal@gmail.com>
2022-10-29 21:05:38 +0100

    Initial kobweb (kotlin compose)

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+    alias(libs.plugins.kobwebx.markdown)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+    maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+}
+
+group = "com.bonnjalal.jportfolio"
+version = "1.0-SNAPSHOT"
+
+kobweb {
+    index {
+        description.set("Powered by Kobweb")
+    }
+}
+
+kotlin {
+    jvm {
+        tasks.withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = "11"
+        }
+
+        tasks.named("jvmJar", Jar::class.java).configure {
+            archiveFileName.set("jportfolio.jar")
+        }
+    }
+    js(IR) {
+        moduleName = "jportfolio"
+        browser {
+            commonWebpackConfig {
+                outputFileName = "jportfolio.js"
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(libs.kobweb.core)
+                implementation(libs.kobweb.silk.core)
+                implementation(libs.kobweb.silk.icons.fa)
+                implementation(libs.kobwebx.markdown)
+             }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(libs.kobweb.api)
+             }
+        }
+    }
+}
